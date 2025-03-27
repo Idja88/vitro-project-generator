18b966da81a76cd1c1bed723cbca63ce894b16ec
Idja88 <idja08@gmail.com>
2025-03-27 17:34:45 +0500

    Project Structure ReAdded

--- routes/set.py
+++ routes/set.py
@@ -26,6 +26,51 @@ def create_new_project(token, project_id):
 
     if not project_folder_data:
         return jsonify({"error": "Не удалось создать папку проекта"}), 500
+    
+    if isinstance(project_data.get('selectionMatrix'), str):
+        selection_matrix = jsonify(project_data.get('selectionMatrix'))
+
+    for object_folder in selection_matrix['objects']:
+
+        if object_folder['id'] == '00000000-0000-0000-0000-000000000000':
+            object_folder_income_data = [{ 
+                "list_id" : current_app.config['DOCUMENT_LIST_ID'],
+                "parent_id": project_folder_data[0]['id'],
+                "content_type_id" : current_app.config['OBJECT_FOLDER_CT_ID'],
+                "name": object_folder['name']
+            }]
+        else:
+            object_folder_income_data = [{
+            "list_id" : current_app.config['DOCUMENT_LIST_ID'],
+            "parent_id": project_folder_data[0]['id'],
+            "content_type_id" : current_app.config['OBJECT_FOLDER_CT_ID'],
+            "name": object_folder['name'],
+            "object_list_lookup": object_folder['id']
+        }]
+        
+        object_data = vc.update_mp_list(token, object_folder_income_data)
+
+        if not object_data:
+            return jsonify({"error": "Не удалось создать объект проекта"}), 500
+
+        for mark_folder in object_folder['marks']:
+
+            if mark_folder['number'] == '':
+                mark_folder['number'] = None
+
+            mark_folder_income_data = [{
+                "list_id" : current_app.config['DOCUMENT_LIST_ID'],
+                "parent_id": object_data[0]['id'],
+                "content_type_id" : current_app.config['MARK_FOLDER_CT_ID'],
+                "name": mark_folder['name'],
+                "sheet_set_lookup": mark_folder['id'],
+                "sheet_set_number": mark_folder['number']
+            }]
+
+            mark_data = vc.update_mp_list(token, mark_folder_income_data)
+
+            if not mark_data:
+                return jsonify({"error": "Не удалось создать марку проекта"}), 500
 
     return jsonify({"message": "Проект успешно создан", "projectId": project_folder_data[0]['id']}), 201 # Возвращаем ID нового проекта
 
