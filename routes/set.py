162007a14980fded76e6966b038cedeb3a74b4db
Idja88 <idja08@gmail.com>
2025-04-02 14:17:06 +0500

    Added mark creation for special object

--- routes/set.py
+++ routes/set.py
@@ -55,43 +55,57 @@ def create_new_project(token, project_id):
     for object_folder in selection_matrix['objects']:
 
         if object_folder['id'] == '00000000-0000-0000-0000-000000000000':
-            object_folder_income_data = [{ 
-                "list_id" : current_app.config['DOCUMENT_LIST_ID'],
-                "parent_id": project_folder_data[0]['id'],
-                "content_type_id" : current_app.config['OBJECT_FOLDER_CT_ID'],
-                "name": object_folder['name']
-            }]
+            # Для специального объекта создаем марки напрямую внутри папки проекта
+            for mark_folder in object_folder['marks']:
+
+                if mark_folder['number'] == '':
+                    mark_folder['number'] = None
+
+                mark_folder_income_data = [{
+                    "list_id": current_app.config['DOCUMENT_LIST_ID'],
+                    "parent_id": project_folder_data[0]['id'],  # Используем папку проекта как родительскую
+                    "content_type_id": current_app.config['MARK_FOLDER_CT_ID'],
+                    "name": mark_folder['name'],
+                    "sheet_set_lookup": mark_folder['id'],
+                    "sheet_set_number": mark_folder['number']
+                }]
+
+                mark_data = vc.update_mp_list(token, mark_folder_income_data)
+
+                if not mark_data:
+                    return jsonify({"error": "Не удалось создать марку проекта"}), 500
         else:
+
             object_folder_income_data = [{
             "list_id" : current_app.config['DOCUMENT_LIST_ID'],
             "parent_id": project_folder_data[0]['id'],
             "content_type_id" : current_app.config['OBJECT_FOLDER_CT_ID'],
             "name": object_folder['name'],
             "object_list_lookup": object_folder['id']
-        }]
-        
-        object_data = vc.update_mp_list(token, object_folder_income_data)
-
-        if not object_data:
-            return jsonify({"error": "Не удалось создать объект проекта"}), 500
-
-        for mark_folder in object_folder['marks']:
-
-            if mark_folder['number'] == '':
-                mark_folder['number'] = None
-
-            mark_folder_income_data = [{
-                "list_id" : current_app.config['DOCUMENT_LIST_ID'],
-                "parent_id": object_data[0]['id'],
-                "content_type_id" : current_app.config['MARK_FOLDER_CT_ID'],
-                "name": mark_folder['name'],
-                "sheet_set_lookup": mark_folder['id'],
-                "sheet_set_number": mark_folder['number']
             }]
+        
+            object_data = vc.update_mp_list(token, object_folder_income_data)
 
-            mark_data = vc.update_mp_list(token, mark_folder_income_data)
+            if not object_data:
+                return jsonify({"error": "Не удалось создать объект проекта"}), 500
 
-            if not mark_data:
-                return jsonify({"error": "Не удалось создать марку проекта"}), 500
+            for mark_folder in object_folder['marks']:
+
+                if mark_folder['number'] == '':
+                    mark_folder['number'] = None
+
+                mark_folder_income_data = [{
+                    "list_id" : current_app.config['DOCUMENT_LIST_ID'],
+                    "parent_id": object_data[0]['id'],
+                    "content_type_id" : current_app.config['MARK_FOLDER_CT_ID'],
+                    "name": mark_folder['name'],
+                    "sheet_set_lookup": mark_folder['id'],
+                    "sheet_set_number": mark_folder['number']
+                }]
+
+                mark_data = vc.update_mp_list(token, mark_folder_income_data)
+
+                if not mark_data:
+                    return jsonify({"error": "Не удалось создать марку проекта"}), 500
 
     return jsonify(project_folder_data), 201 # Возвращаем ID нового проекта
