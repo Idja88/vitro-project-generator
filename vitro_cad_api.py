e3b07a2599c2e711b9e0ef27cf17ac4dd38866a5
Idja88 <idja08@gmail.com>
2025-04-10 17:05:47 +0500

    Added new Delete API call

--- vitro_cad_api.py
+++ vitro_cad_api.py
@@ -68,7 +68,8 @@ def get_mp_children(mp_token, parent_id, recursive=False, query=None):
     except requests.exceptions.RequestException as e:
         print(f"Error getting MP children (parent_id: {parent_id}, recursive: {recursive}): {e}")
         return None
-    
+
+# Копируем элемент списка в Vitro-CAD MP
 def copy_mp_item(mp_token, to_item_id, data):
     mp_url = current_app.config['VITRO_CAD_API_BASE_URL']
     url_string = f"{mp_url}/api/item/copy/{to_item_id}"
@@ -78,4 +79,16 @@ def copy_mp_item(mp_token, to_item_id, data):
             response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Error copying MP item (item_id: {to_item_id}): {e}")
+        return None
+
+# Удаляем элемент списка в Vitro-CAD MP
+def delete_mp_item(mp_token, data):
+    mp_url = current_app.config['VITRO_CAD_API_BASE_URL']
+    url_string = f"{mp_url}/api/item/delete"
+    headers = {'Authorization': mp_token}
+    try:
+        with requests.post(url=url_string, headers=headers, json=data) as response:
+            response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error deleting MP items: {e}")
         return None
